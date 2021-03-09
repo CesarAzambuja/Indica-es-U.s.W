c9a7caf36273378205524c5ffe0632a7e477bb95
César Oliveira <cesar.oliveira@takelog.com.br>
2021-03-08 23:09:24 -0300

    Adicionando cursos free 4

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const dados = [
     {
         id: true,
         title: "Curso em Video - HTML5 / CSS / JS",
-        image: "img/html.PNG",
+        image: "img/html.jpg",
         price: "Acessar",
         url_repo: "https://www.youtube.com/watch?v=epDCjksKMok&list=PLHz_AreHm4dlAnJ_jJtV29RFxnPHDuk9o"
     },
@@ -19,7 +19,7 @@ const dados = [
     {
         id: true,
         title: "Curso em Video - Lógica de Programação",
-        image: "img/cursoenvideo.PNG",
+        image: "img/cursoemvideo.jpg",
         price: "Acessar",
         url_repo: "https://www.youtube.com/watch?v=8mei6uVttho&list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV"
     },
